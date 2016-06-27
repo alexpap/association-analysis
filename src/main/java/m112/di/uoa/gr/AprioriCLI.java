f613640d2b3c3797d721b926b6f786a4acf6033a
alex <alpap@di.uoa.gr>
2016-06-27 14:40:37 +0300

    skip gen rules for cli

--- src/main/java/m112/di/uoa/gr/AprioriCLI.java
+++ src/main/java/m112/di/uoa/gr/AprioriCLI.java
@@ -106,14 +106,14 @@ public class AprioriCLI {
             }
         }
 
-        List<AprioriRule> rules_temp;
-        List<AprioriRule> rules_all=new ArrayList();
-        AprioriAssociationRulesGeneration rules_gen = new AprioriAssociationRulesGeneration(trees, minconf, rules_all);
-        while (rules_gen.hasNext()) {
-            rules_temp=rules_gen.next();
-            for (int i=0; i<rules_temp.size(); i++) {
-                log.debug(rules_temp.get(i).toString());
-            }
-        }
+//        List<AprioriRule> rules_temp;
+//        List<AprioriRule> rules_all=new ArrayList();
+//        AprioriAssociationRulesGeneration rules_gen = new AprioriAssociationRulesGeneration(trees, minconf, rules_all);
+//        while (rules_gen.hasNext()) {
+//            rules_temp=rules_gen.next();
+//            for (int i=0; i<rules_temp.size(); i++) {
+//                log.debug(rules_temp.get(i).toString());
+//            }
+//        }
     }
 }
